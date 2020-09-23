7951fdea57fec4dce2c268eccf8efe7291d06368
3xter <3x-ter@mail.ru>
2020-09-23 21:57:34 +0300

    Fix selectClub

--- src/modules/selectClub.js
+++ src/modules/selectClub.js
@@ -3,12 +3,16 @@
 const selectClub = () => {
 
     const body = document.body
+    const head = document.querySelector('.head');
     const clubsList = document.querySelector('.clubs-list>ul')
         body.addEventListener('click', (event) => {
             let target = event.target
+            console.log(target)
             if (target.closest('.club-select') && getComputedStyle(clubsList).display === 'none') {
                 clubsList.style.display = 'block'
-            } else clubsList.style.display = ''
+            } else if (!target.closest('.clubs-list>ul')) {
+                clubsList.style.display = ''
+            }
     })
 }
 
