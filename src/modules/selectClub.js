fbd1e6035ef408df3caa2121ac4deca1da38438b
3xter <3x-ter@mail.ru>
2020-09-23 12:45:29 +0300

    Did selectClub

--- src/modules/selectClub.js
+++ src/modules/selectClub.js
@@ -1,7 +1,17 @@
 'use strict';
 
 const selectClub = () => {
-    console.log('asfsdf')
+
+    const body = document.body
+    const clubsList = document.querySelector('.clubs-list>ul')
+        body.addEventListener('click', (event) => {
+            let target = event.target
+            console.log(target)
+            console.log(getComputedStyle(clubsList).display);
+            if (target.closest('.club-select') && getComputedStyle(clubsList).display === 'none') {
+                clubsList.style.display = 'block'
+            } else clubsList.style.display = ''
+    })
 }
 
 
