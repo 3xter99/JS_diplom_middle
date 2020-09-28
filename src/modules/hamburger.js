2f8590b4bcde0746cc933b413f3b28371448fd2b
3xter <3x-ter@mail.ru>
2020-09-28 20:30:33 +0300

    fix hamburger

--- src/modules/hamburger.js
+++ src/modules/hamburger.js
@@ -1,6 +1,12 @@
 const hamburger = () => {
     const topMenu = document.querySelector('.top-menu'),
         headerMain = document.querySelector('.header-main');
+    if (window.innerWidth <= 767) {
+        topMenu.style.position = 'fixed'
+        headerMain.style.marginTop = '60px'
+    } else {
+        topMenu.style.position = ''
+        headerMain.style.marginTop = ''}
     window.addEventListener('resize', () => {
         if (window.innerWidth <= 767) {
             topMenu.style.position = 'fixed'
